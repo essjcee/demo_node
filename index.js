1c2a4f727e0e9d71c9d494dfc27611bab2f07454
John Cole <stuartjohncole@gmail.com>
2019-02-06 00:26:11 +0000

    second commit

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express();
 //Respond with "hello world" for requests that hit our root "/"
 app.get('/', (req, res) => {
- res.send('Hello from node');
+ res.send('Hello World from node');
 });
 //listen to port 3000 by default
 app.listen(3000);
